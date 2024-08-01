1333c64f54a36422e132c5e44d267b327f2574d2
Bemin Dawoud <bemin_dawoud@yahoo.com>
2024-08-02 02:20:31 +0300

    create tasks from 1 to 6

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+Task 2.
+'''
+
+
+def floor(a: float) -> int:
+    '''
+    Computes the floor.
+    '''
+    return int(a)
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''Task 3.
+'''
+
+
+def to_str(n: float) -> str:
+    '''
+    returns a tring representation of a float.
+    '''
+    return str(n)
