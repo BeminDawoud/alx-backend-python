47aea65eb6fd75caac11c735d39db97d03bb77bd
Bemin Dawoud <bemin_dawoud@yahoo.com>
2024-08-07 23:57:30 +0300

    fix type hinting

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,7 +7,10 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> Generator:
+async def async_generator() -> Generator[float, None, None]:
+    '''
+    task 0.
+    '''
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
